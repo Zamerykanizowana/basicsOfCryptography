9341792f2f5671f8fa9163c3c517928da9de3385
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2021-12-04 13:55:16 +0100

    Put bcd, cbc, pcbc into def

--- taskNo2/main.py
+++ taskNo2/main.py
@@ -2,6 +2,7 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 import random
 from copy import deepcopy
+from datetime import datetime
 
 
 def chunk(m, size):
@@ -31,7 +32,7 @@ def test_ciper_encryption_decryption(en_m, de_m, mode):
 def all_tests(secret_message):
     def _remove_chunk(s_m,idx):
         sec_m = deepcopy(s_m)
-        print(f'\x1b[33mtest line: sec_m: {sec_m}\x1b[0m')
+        #print(f'\x1b[33mtest line: sec_m: {sec_m}\x1b[0m')
         del sec_m[idx]
         return sec_m
 
@@ -46,8 +47,8 @@ def all_tests(secret_message):
     def _swap_chunks(s_m,idx):
         sec_m = deepcopy(s_m)
         # swap with next chunk if possible, else with previous
-        print(f'\x1b[33mtest line: idx: {idx} len(sec_m): {len(sec_m)}\x1b[0m')
-        print(f'\x1b[33mtest line: sec_m: {sec_m}\x1b[0m')
+        #print(f'\x1b[33mtest line: idx: {idx} len(sec_m): {len(sec_m)}\x1b[0m')
+        #print(f'\x1b[33mtest line: sec_m: {sec_m}\x1b[0m')
         if idx == len(sec_m)-2:
             sec_m[idx], sec_m[idx+1] = sec_m[idx+1], sec_m[idx]
         else:
@@ -57,7 +58,7 @@ def all_tests(secret_message):
     def _change_byte_in_chunk(s_m, idx):
         sec_m = deepcopy(s_m)
         byte_idx_to_change = random.randint(0,len(sec_m[0])-1)
-        print(f'all_tests: _change_byte_in_chunk: byte_idx_to_change: {byte_idx_to_change}')
+        #print(f'all_tests: _change_byte_in_chunk: byte_idx_to_change: {byte_idx_to_change}')
         new_chunk = b''.join([b'\x00'*byte_idx_to_change, b'r', b'\x00'*(15-byte_idx_to_change)])
         sec_m[idx] = new_chunk
         return sec_m
@@ -84,88 +85,179 @@ def all_tests(secret_message):
     cha_b_test = _change_byte_in_chunk(secret_message,selected_chunk_idx)
     swa_b_test = _swap_bytes_in_chunk(secret_message,selected_chunk_idx)
     rem_b_test = _remove_byte_in_chunk(secret_message,selected_chunk_idx)
-    print('_remove_chunk result:')
-    print(rem_ch_test)
-    print('_double_chunk result:')
-    print(dou_ch_test)
-    print('_swap_chunk result:')
-    print(swa_ch_test)
-    print('_change_byte_in_chunk result:')
-    print(cha_b_test)
-    print('_swap_byte_in_chunk result:')
-    print(swa_b_test)
-    print('_remove_byte_in_chunk result:')
-    print(rem_b_test)
+    print_result = False
+    if print_result:
+        print('_remove_chunk result:')
+        print(rem_ch_test)
+        print('_double_chunk result:')
+        print(dou_ch_test)
+        print('_swap_chunk result:')
+        print(swa_ch_test)
+        print('_change_byte_in_chunk result:')
+        print(cha_b_test)
+        print('_swap_byte_in_chunk result:')
+        print(swa_b_test)
+        print('_remove_byte_in_chunk result:')
+        print(rem_b_test)
+    tests_names = ['_remove_chunk', '_double_chunk', '_swap_chunk', '_change_byte_in_chunk',\
+            '_swap_byte_in_chunk', '_remove_byte_in_chunk']
+    tests_res = [rem_ch_test, dou_ch_test, swa_ch_test, cha_b_test, swa_b_test, rem_b_test]
+    return [tests_names, tests_res]
 
+def ecb(message_to_encrypt, k, f): 
+    def _encrypt(pad_message_in_chunks, key):
+        encrypted_message = []
+        for ele in pad_message_in_chunks:
+            encrypted_message.append(encrypt_chunk(ele,key))
+        print(f'encrypted_message:\n{encrypted_message}')
+        return encrypted_message
+
+    def _decrypt(encrypted_message, key):
+        decrypted_message = []
+        for ele in encrypted_message:
+            decrypted_message.append(decrypt_chunk(ele,key))
+        return decrypted_message
+
+    pad_message_in_chunk = chunk(pad(message_to_encrypt,16),16)
     
+    encrypted_message = _encrypt(pad_message_in_chunk, k)
+    decrypted_message = _decrypt(encrypted_message, k)
     
-# message to test
-message_to_encrypt = b'I just want to finish this task and go to bed.'
-# key
+    decrypted_message_human_readable = unpad(unchunk(decrypted_message),16)
+    print(decrypted_message_human_readable)
+    f.write('ECB encrypted_message: ' + str(encrypted_message) + '\n')
+    f.write('ECB decrypted_message_human_readable: ' + decrypted_message_human_readable.decode('utf-8') + '\n')
+    test_ciper_encryption_decryption(message_to_encrypt, decrypted_message_human_readable,'ECB')
+    
+    tests_result = all_tests(encrypted_message)
+    #print(f'tests_result:\n{tests_result}')
+    print('------------------- tests result -------------------')
+    for idx, ele in enumerate(tests_result[1]):
+        print(f'{tests_result[0][idx]}')
+        f.write('ECB test: ' + tests_result[0][idx] + '\n')
+        try:
+            tmp_dec = unchunk(_decrypt(ele,k))
+            print(tmp_dec)
+            f.write(tmp_dec.decode('ISO-8859-1') + '\n')
+        except ValueError as e:
+            m = "Oops! Value can't be decrypt properly :C"
+            print(e)
+            f.write(m + '\n')
+
+def cbc(message_to_encrypt, k, f, initialization_vector):
+    def _encrypt(pad_message_in_chunk, key, i_vector):
+        encrypted_message = []
+        for ele in pad_message_in_chunk:
+            if len(encrypted_message) == 0:
+                encrypted_message.append(encrypt_chunk(xor_chunks(ele,i_vector),cipherKey))
+            else:
+                encrypted_message.append(encrypt_chunk(xor_chunks(ele,encrypted_message[-1]),cipherKey))
+        return encrypted_message
+
+    def _decrypt(encrypted_message, key, i_vector):
+        decrypted_message = []
+        for idx, ele in enumerate(encrypted_message):
+            if len(decrypted_message) == 0:
+                decrypted_message.append(xor_chunks(decrypt_chunk(ele,cipherKey),i_vector))
+            else:
+                decrypted_message.append(xor_chunks(decrypt_chunk(ele,cipherKey),encrypted_message[idx-1]))
+        return decrypted_message
+
+    pad_message_in_chunk = chunk(pad(message_to_encrypt,16),16)
+    
+    encrypted_message = _encrypt(pad_message_in_chunk, k, initialization_vector)
+    decrypted_message = _decrypt(encrypted_message, k, initialization_vector)
+    
+    decrypted_message_human_readable = unpad(unchunk(decrypted_message),16)
+    
+    f.write('CBC encrypted_message: ' + str(encrypted_message) + '\n')
+    f.write('CBC decrypted_message_human_readable: ' + decrypted_message_human_readable.decode('utf-8') + '\n')
+    test_ciper_encryption_decryption(message_to_encrypt, decrypted_message_human_readable,'CBC')
+    
+    tests_result = all_tests(encrypted_message)
+    for idx, ele in enumerate(tests_result[1]):
+        print(f'{tests_result[0][idx]}')
+        f.write('CBC test: ' + tests_result[0][idx] + '\n')
+        try:
+            tmp_dec = unchunk(_decrypt(ele,k,initialization_vector))
+            print(tmp_dec)
+            f.write(tmp_dec.decode('ISO-8859-1') + '\n')
+        except ValueError as e:
+            m = "Oops! Value can't be decrypt properly :C"
+            print(e)
+            f.write(m + '\n')
+
+def pcbc(message_to_encrypt, k, f, initialization_vector):
+    def _encrypt(pad_message_in_chunk, key, i_vector): 
+        encrypted_message = []
+        helper_tab = []
+        for ele in pad_message_in_chunks:
+            if len(encrypted_message) == 0:
+                encrypted_message.append(encrypt_chunk(xor_chunks(ele,i_vector), key))
+            else: 
+                encrypted_message.append(encrypt_chunk(xor_chunks(ele, helper_tab[-1]), key))
+            helper_tab.append(xor_chunks(encrypted_message[-1],ele))
+        return encrypted_message
+
+    def _decrypt(encrypted_message, key, i_vector):
+        decrypted_message = []
+        helper_tab = []
+        for idx, ele in enumerate(encrypted_message):
+            if len(decrypted_message) == 0:
+                decrypted_message.append(xor_chunks(decrypt_chunk(ele,key),i_vector))
+            else:
+                decrypted_message.append(xor_chunks(decrypt_chunk(ele,key),helper_tab[-1]))
+            helper_tab.append(xor_chunks(decrypted_message[-1],ele))
+        return decrypted_message
+
+    pad_message_in_chunks = chunk(pad(message_to_encrypt,16),16)
+    
+    encrypted_message = _encrypt(pad_message_in_chunks, k, initialization_vector)
+    decrypted_message = _decrypt(encrypted_message, k, initialization_vector)
+    
+    decrypted_message_human_readable = unpad(unchunk(decrypted_message),16)
+    
+    f.write('PCBC encrypted_message: ' + str(encrypted_message) + '\n')
+    f.write('PCBC decrypted_message_human_readable: ' + decrypted_message_human_readable.decode('utf-8') + '\n')
+    test_ciper_encryption_decryption(message_to_encrypt, decrypted_message_human_readable,'PCBC')
+    
+    tests_result = all_tests(encrypted_message)
+    for idx, ele in enumerate(tests_result[1]):
+        print(f'{tests_result[0][idx]}')
+        f.write('PCBC test: ' + tests_result[0][idx] + '\n')
+        try:
+            tmp_dec = unchunk(_decrypt(ele,k,initialization_vector))
+            print(tmp_dec)
+            f.write(tmp_dec.decode('ISO-8859-1') + '\n')
+        except ValueError as e:
+            m = "Oops! Value can't be decrypt properly :C"
+            print(e)
+            f.write(m + '\n')
+
+
+
+
+
+message = b'I just want to finish this task and go to bed.'
 cipherKey = b'oskm3ucjakops2LA'
-print(f'length mes: {len(message_to_encrypt)}')
-pad_message = pad(message_to_encrypt,16)
-print(f'length of pad_message: {len(pad_message)}')
+initialization_vector = b'ajsujnghkopkjasv'
+
+now = datetime.now()
+date_and_time = now.strftime("%d-%m-%Y_%H:%M:%S")
+file_name = date_and_time + "_data.txt"
+file_data = open(file_name, 'w+')
+file_data.write('message to encrypt: ' + message.decode('utf-8') + '\n')
+
+ecb(message, cipherKey, file_data)
+cbc(message, cipherKey, file_data, initialization_vector)
+pcbc(message, cipherKey, file_data, initialization_vector)
+
+file_data.close()
+#print(f'length mes: {len(message)}')
+pad_message = pad(message,16)
+#print(f'length of pad_message: {len(pad_message)}')
 #print(f'pad_message: {pad_message}')
 pad_message_in_chunks = chunk(pad_message, 16)
 #print(f'pad_message_in_chunks: {pad_message_in_chunks}')
-
-# ECB
-encrypted_message = []
-for ele in pad_message_in_chunks:
-    encrypted_message.append(encrypt_chunk(ele,cipherKey))
-print(f'encrypted_message:\n{encrypted_message}')
-
-all_tests(encrypted_message)
-
-decrypted_message = []
-for ele in encrypted_message:
-    decrypted_message.append(decrypt_chunk(ele,cipherKey))
-test_ciper_encryption_decryption(message_to_encrypt, unpad(unchunk(decrypted_message),16),'ECB')
-
-# CBC
-encrypted_message = []
-initialization_vector = b'ajsujnghkopkjasv'
-for ele in pad_message_in_chunks:
-    if len(encrypted_message) == 0:
-        encrypted_message.append(encrypt_chunk(xor_chunks(ele,initialization_vector),cipherKey))
-    else:
-        encrypted_message.append(encrypt_chunk(xor_chunks(ele,encrypted_message[-1]),cipherKey))
-
-decrypted_message = []
-for idx, ele in enumerate(encrypted_message):
-    if len(decrypted_message) == 0:
-        decrypted_message.append(xor_chunks(decrypt_chunk(ele,cipherKey),initialization_vector))
-    else:
-        decrypted_message.append(xor_chunks(decrypt_chunk(ele,cipherKey),encrypted_message[idx-1]))
-test_ciper_encryption_decryption(message_to_encrypt, unpad(unchunk(decrypted_message),16),'CBC')
-
-
-# PCBC
-encrypted_message = []
-helper_tab = []
-for ele in pad_message_in_chunks:
-    if len(encrypted_message) == 0:
-        encrypted_message.append(encrypt_chunk(xor_chunks(ele,initialization_vector),cipherKey))
-    else: 
-        encrypted_message.append(encrypt_chunk(xor_chunks(ele, helper_tab[-1]),cipherKey))
-    helper_tab.append(xor_chunks(encrypted_message[-1],ele))
-
-decrypted_message = []
-helper_tab = []
-for idx, ele in enumerate(encrypted_message):
-    if len(decrypted_message) == 0:
-        decrypted_message.append(xor_chunks(decrypt_chunk(ele,cipherKey),initialization_vector))
-    else:
-        decrypted_message.append(xor_chunks(decrypt_chunk(ele,cipherKey),helper_tab[-1]))
-    helper_tab.append(xor_chunks(decrypted_message[-1],ele))
-test_ciper_encryption_decryption(message_to_encrypt, unpad(unchunk(decrypted_message),16),'PCBC')
-
-
-
-
-
-
-
 
 
