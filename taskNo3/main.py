f3b4605c57f45e4aed9e72848db983bf4883c1b6
Aleksandra Wojewoda <a.wojewoda18@gmail.com>
2021-12-16 01:18:12 +0100

    Works for tested values

--- taskNo3/main.py
+++ taskNo3/main.py
@@ -1,20 +1,19 @@
 import random
 import math
 
-def c_print(string, mode):
+def c_str(string, mode):
     if mode == 'err':
-        print('\x1b[31m' + str(string) + '\x1b[0m')
+        return '\x1b[31m' + str(string) + '\x1b[0m'
     elif mode == 'res':
-        print('\x1b[32m' + str(string) + '\x1b[0m')
+        return '\x1b[32m' + str(string) + '\x1b[0m'
     elif mode == 'war':
-        print('\x1b[33m' + str(string) + '\x1b[0m')
-    elif mode == 'debb':
-        print('\x1b[34m' + str(string) + '\x1b[0m')
-    elif mode == 'debv':
-        print('\x1b[35m' + str(string) + '\x1b[0m')
+        return '\x1b[33m' + str(string) + '\x1b[0m'
+    elif mode == 'deb':
+        return '\x1b[34m' + str(string) + '\x1b[0m'
+    elif mode == 'inte':
+        return '\x1b[35m' + str(string) + '\x1b[0m'
     elif mode == 'tit':
-        print('\x1b[36m' + str(string) + '\x1b[0m')
-
+        return '\x1b[36m' + str(string) + '\x1b[0m'
 
 def prime_number(num):
     if num > 1:
@@ -26,23 +25,62 @@ def prime_number(num):
     else:
         return False
 
+def xea(e, fi):
+    if e == 0:
+        return 0, 1
+    x, y = xea(fi%e,e)
+    return y-(fi//e)*x, x
+
+
 def generate_key():
-    c_print('----------Key generator------------', 'tit')
+    print(c_str('----------Key generator------------', 'tit'))
     p = 0
     while not prime_number(p):
-        p = random.randint(30,200)
+        p = random.randint(1,100)
     q = 0
     while not prime_number(q):
-        q = random.randint(30,200)
-    c_print(f'p: {p}', 'debb')
-    c_print(f'q: {q}', 'debb')
+        q = random.randint(1,100)
+    p = 31
+    q = 19
+    print(c_str(f'p: {p}', 'deb'))
+    print(c_str(f'q: {q}', 'deb'))
     n = p * q
-    c_print(f'n: {n}', 'debb')
+    print(c_str(f'n: {n}', 'deb'))
     fi_n = (p-1)*(q-1)
-    c_print(f'fi_n: {fi_n}', 'debb')
+    print(c_str(f'fi_n: {fi_n}', 'deb'))
     e = 0
     while not prime_number(e) or not math.gcd(e, fi_n) == 1:
-        e = random.randint(0,300)
-    c_print(f'e: {e}', 'debb')
+        e = random.randint(0,200)
+    e = 7
+    print(c_str(f'e: {e}', 'deb'))
+    d, _ = xea(e,fi_n)
+    d = d%fi_n
+    print(c_str(f'd: {d}', 'deb'))
+    print(c_str(f'public key: {e} and {n}, private key: {d} i {n}', 'res'))
+    return e, n, d
+    
 
-generate_key()
+def encrypt(e, n):
+    question = input(c_str('Press m if manual message to encrypt, press a to automatic\n','inte'))
+    if question == 'a':
+        m = random.randint(1,9999)
+        m = 8
+    elif question == 'm':
+        m = 0
+        while m < 1 or m > 9999:  
+            m = int(input(c_str('Select number from 1 to 9999\n', 'inte')))
+    print(c_str(f'm: {m}', 'deb'))
+    c = (m**e) % n
+    print(c_str(f'encrypt message c: {c}','res'))
+    return c
+
+def decrypt(d, n, c):
+    m = c**d % n
+    print(c_str(f'decrypted message: m: {m}', 'res'))
+    return m
+
+e, n, d = generate_key()
+print(f'{e}, {n}, {d}')
+c = encrypt(e, n)
+print(f'{c}')
+m = decrypt(d, n, c)
